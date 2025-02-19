c75d5b21cfc32ec56630ab192fe80602fda9e589
Anshul Karyal <karyalanshul@gmail.com>
2025-02-19 17:02:55 +0530

    Added Mongodb cluster

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 // db.js
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const MONGO_URI = 'mongodb+srv://anuj:anuj@cluster0.c7osm.mongodb.net/';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(process.env.DATABASE_URL);
         console.log('MongoDB connected successfully');
     } catch (err) {
         console.error('MongoDB connection error:', err.message);
