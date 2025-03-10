97a9c2ab71e79a576ebe6b94bd40380fdd347605
CodeVerseByAnuj <anuj.mmmut@gmail.com>
2025-03-10 15:40:08 +0530

    updated for cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const batchRouter=require('./router/batch.router');
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:3001', 'https://your-frontend-domain.com','http://localhost:5173'], // Add your frontend URLs here
+  origin: ['http://localhost:3001', 'https://nsdc-frontend.vercel.app','http://localhost:5173'], // Add your frontend URLs here
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the allowed methods
   allowedHeaders: 'Content-Type,Authorization,x-csrf-token',
   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
